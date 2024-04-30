b403e5f7db5bad36f052894ce6415be6477b3f49
orlandeSSFS <orlande@ssfs.org>
2024-04-30 14:52:41 -0400

    Initial Commit

---
+++ AccessibleAudioPlayer/AccessibleAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibleAudioPlayerApp.swift
+//  AccessibleAudioPlayer
+//
+//  Created by orlande on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibleAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
